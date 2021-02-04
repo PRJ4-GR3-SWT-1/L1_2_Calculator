05c00e0f7ab8c3d8788b8ffbb62370f34b18dd0e
Laurits H.G <201905745@post.au.dk>
2021-02-04 15:20:06 +0100

    Overloaded Multiply, Power and Divide

--- L1_Calculator/Calculator.cs
+++ L1_Calculator/Calculator.cs
@@ -20,7 +20,6 @@ namespace L1_Calculator
         {
             return a + b;
         }
-
         public double Subtract(double a, double b)
         {
             return a - b;
@@ -28,17 +27,36 @@ namespace L1_Calculator
 
         public double Multiply(double a, double b)
         {
+            Accumulator = a * b;
             return a * b;
         }
-
+        public double Multiply(double numberEnd)
+        {
+            Accumulator = Accumulator * numberEnd;
+            return Accumulator;
+        }
         public double Power(double x, double exp)
         {
+            Accumulator = Math.Pow(x, exp);
             return Math.Pow(x, exp);
+        }
+        public double Power(double exp)
+        {
+            double result = Math.Pow(Accumulator, exp);
+            Accumulator = result;
+            return result;
         }
 
         public double divide(double a, double b)
         {
             double result = (a / b);
+            Accumulator = result;
+            return result;
+        }
+        public double divide(double aEnd)
+        {
+            double result = (Accumulator / aEnd);
+            Accumulator = result;
             return result;
         }
 
