02c97cf8492ba6eee6a2a185275ec1ef428b3aef
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-02-11 13:00:57 +0100

    Slettet console.write("Hello World")

--- L1_Calculator/Calculator.cs
+++ L1_Calculator/Calculator.cs
@@ -6,7 +6,7 @@ namespace L1_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
 
         public double Accumulator{get; private set; }
